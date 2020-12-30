9394942830414b2a820ad37adb061f648cb61b09
mahdihajizadehofficial <75172352+mahdihajizadehofficial@users.noreply.github.com>
2020-12-30 11:13:49 -0800

    updating...

--- app/botCode.py
+++ app/botCode.py
@@ -6,11 +6,11 @@ bot = TelegramClient('bot',api_id,api_hash).start(bot_token)
 @bot.on(events.NewMessage(pattern='/start'))
 async def send_welcome(event):
     await event.reply('Howdy, how are you doing?')
-
+     
 @bot.on(events.NewMessage)
 async def echo_all(event):
     await event.reply(event.text)
 
 def serve():
   with bot:
-    bot.run_until_disconnected()
+    bot.run_until_disconnected()    
